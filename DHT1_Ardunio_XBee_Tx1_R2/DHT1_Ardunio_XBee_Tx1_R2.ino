25bb2542a351e3138806d94ac07b1487613b5dc3
Nandkishor Dhawale <107688392+drnmdhawale@users.noreply.github.com>
2022-07-05 18:37:35 +0530

    Update DHT1_Ardunio_XBee_Tx1_R2.ino
    -u

--- DHT1_Ardunio_XBee_Tx1_R2/DHT1_Ardunio_XBee_Tx1_R2.ino
+++ DHT1_Ardunio_XBee_Tx1_R2/DHT1_Ardunio_XBee_Tx1_R2.ino
@@ -2,7 +2,11 @@
 // To trasmitt the aquired data using the XBEE RF module (router 2) 
 // over serial communication link
 // Created by Dr. Nandkishor Motiram Dhawale and Kiran Ashok Shinde
-// Date of Creation Sep 2020
+// Date of Creation Dec 2020
+// Project Name :  aquire sensor data from DHT22 Sensor
+// Recreated and Implemented by :- Kiran Shinde and Rohan Kulkarni. For PVPIT Budhgaon, I/C - R&D Cell
+// Under Guidence of Dr.N.M.Dhawale, I/c- R&D Cell, P.V.P.I.T Budhgaon, Sangli, India in Dec 2020 
+
 
 #include "DHT.h"
 #define DHTPIN 2
